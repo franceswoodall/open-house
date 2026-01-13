cf8ab0db02fe61dfde71ef6ecaf053df36ba4081
Frances Woodall <franceswoodall@hotmail.co.uk>
2026-01-13 15:41:14 +0000

    built listings landing page

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,3 +4,14 @@ const router = express.Router();
 const Listing = require('../models/listing');
 
 module.exports = router;
+
+router.get('/', async (req, res) => {
+    try {
+    const allListings = await Listing.find({}); 
+    console.log(allListings); 
+    res.render('listings/index.ejs'); 
+    } catch (error) {
+        console.log('Error has occured, please try again', error); 
+    }
+  });
+
